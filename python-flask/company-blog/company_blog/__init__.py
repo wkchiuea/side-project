ea6eecc35667ae55af448d27b18f41f18f4c93f5
wkchiuea <wkchiuea@gmail.com>
2024-03-30 21:29:28 +0800

    add: add error handling pages

--- python-flask/company-blog/company_blog/__init__.py
+++ python-flask/company-blog/company_blog/__init__.py
@@ -2,4 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 from company_blog.core.views import core
+from company_blog.error_pages.handlers import error_pages
+
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
