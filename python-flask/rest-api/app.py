1dbffe583cd7c6fb8dae6f0033ae5ddd12a570a1
wkchiuea <wkchiuea@gmail.com>
2024-03-31 12:18:29 +0800

    add: add basic crud api

---
+++ python-flask/rest-api/app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+puppies = []    # [{'name': 'haha'}, ...]
+
+class PuppyNames(Resource):
+
+    def get(self, name):
+        for pup in puppies:
+            if pup['name'] == name:
+                return pup
+        return {'name': None}
+
+    def post(self, name):
+        pup = {'name': name}
+        puppies.append(pup)
+        return pup
+
+    def delete(self, name):
+        for i, pup in enumerate(puppies):
+            if pup['name'] == name:
+                deleted_pup = puppies.pop(i)
+                return {'note': 'delete success'}
+
+class AllNames(Resource):
+
+    def get(self):
+        return {'puppies': puppies}
+
+api.add_resource(PuppyNames, '/puppy/<string:name>')
+api.add_resource(AllNames, '/puppies')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
