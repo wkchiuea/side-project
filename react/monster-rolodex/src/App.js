674a5d9404433c15d107bf6de65c5d28956b02dd
wkchiuea <wkchiuea@gmail.com>
2024-04-04 10:22:17 +0800

    update: use class component, setState

--- react/monster-rolodex/src/App.js
+++ react/monster-rolodex/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: "hehe"
+    }
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Hi, I'm {this.state.name}</p>
+            <button onClick={() => {
+              this.setState(
+                  (state, props) => {
+                    return {
+                      name: "Hoho"
+                    }
+                  },
+                  () => {
+                    console.log(this.state.name);
+                  }
+              )
+            }}>Change Name</button>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
