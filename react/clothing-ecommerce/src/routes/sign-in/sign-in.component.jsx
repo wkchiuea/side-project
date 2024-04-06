0b69441a1b6c5daf76c6bf06e929080769b2f7eb
wkchiuea <wkchiuea@gmail.com>
2024-04-06 14:36:11 +0800

    update: add google auth with redirect

--- react/clothing-ecommerce/src/routes/sign-in/sign-in.component.jsx
+++ react/clothing-ecommerce/src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,20 @@
-import {createUserDocumentFromAuth, signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+    createUserDocumentFromAuth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+
+    useEffect(async () => {
+        const response = await getRedirectResult(auth); // auth is singleton, keep track of all auth states in app
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user)
+        }
+    }, []);
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -11,6 +25,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     );
 }
