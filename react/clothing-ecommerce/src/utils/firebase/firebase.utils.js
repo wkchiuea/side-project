0e09e61d4e8844022aecf422a290b17f95794103
wkchiuea <wkchiuea@gmail.com>
2024-04-07 18:57:20 +0800

    update: use environment variables for firebaseconfig

--- react/clothing-ecommerce/src/utils/firebase/firebase.utils.js
+++ react/clothing-ecommerce/src/utils/firebase/firebase.utils.js
@@ -9,15 +9,13 @@ import {
 } from 'firebase/auth';
 import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore";
 
-
-// dummy config
 const firebaseConfig = {
-    apiKey: "AIzaSyAxvan7VPOG4HPNVSppQY30_Pd3MdeopJs",
-    authDomain: "react-clothing-db-efc31.firebaseapp.com",
-    projectId: "react-clothing-db-efc31",
-    storageBucket: "react-clothing-db-efc31.appspot.com",
-    messagingSenderId: "202402566107",
-    appId: "1:202402566107:web:893cdb2a4f0aec5746e1fe"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
