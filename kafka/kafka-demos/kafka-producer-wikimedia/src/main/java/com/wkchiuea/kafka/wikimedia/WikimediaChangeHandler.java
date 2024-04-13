a3eee02871ecfe86428a4600c5f1ec47166896e8
wkchiuea <wkchiuea@gmail.com>
2024-04-13 14:10:27 +0800

    update: add wikimedia producer part

---
+++ kafka/kafka-demos/kafka-producer-wikimedia/src/main/java/com/wkchiuea/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,47 @@
+package com.wkchiuea.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    KafkaProducer<String, String> kafkaProducer;
+    String topic;
+    private final Logger logger = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() throws Exception {
+        // nothing
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+        kafkaProducer.close();
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+        logger.info(messageEvent.getData());
+        // async
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+        // nothing
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        logger.error("Error in Stream Reading", throwable);
+    }
+}
