cc9e1b758d9f15b8a002c7d224d979fd2e657501
wkchiuea <wkchiuea@gmail.com>
2024-08-09 16:46:25 +0800

    vue-S04: project game over view

--- vue/04-project-monster-slayer/01-prj-monster/app.js
+++ vue/04-project-monster-slayer/01-prj-monster/app.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 1,
+      winner: null,
     }
   },
   computed: {
@@ -15,6 +16,22 @@ const app = Vue.createApp({
     },
     disableSP() {
       return this.currentRound % 3 !== 0;
+    }
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
     }
   },
   methods: {
