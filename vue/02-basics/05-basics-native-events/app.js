7ee0c4c8c2f54ddc3f460ada32971363bb59064e
wkchiuea <wkchiuea@gmail.com>
2024-08-09 11:18:11 +0800

    vue-S02: basics computed properties and watchers

--- vue/02-basics/05-basics-native-events/app.js
+++ vue/02-basics/05-basics-native-events/app.js
@@ -5,6 +5,24 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function (){
+          that.counter = 0;
+        }, 2000);
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + " XX"
+    }
+  },
   methods: {
     setName(event, lastName) {
       this.name = event.target.value;
