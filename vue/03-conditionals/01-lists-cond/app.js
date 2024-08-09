0534a3cad010caac74b65072f4bdd32d73134eec
wkchiuea <wkchiuea@gmail.com>
2024-08-09 15:39:04 +0800

    vue-S03: v-if and v-for

---
+++ vue/03-conditionals/01-lists-cond/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      goalInput: "",
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.goalInput);
+      this.goalInput = "";
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    }
+  }
+});
+
+app.mount('#user-goals');
