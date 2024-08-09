4e97023b469acb2ae554c68f04dc990199fb7771
wkchiuea <wkchiuea@gmail.com>
2024-08-09 10:53:46 +0800

    vue-S02: basics two-way binding

--- vue/02-basics/05-basics-native-events/app.js
+++ vue/02-basics/05-basics-native-events/app.js
@@ -7,7 +7,7 @@ const app = Vue.createApp({
   },
   methods: {
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,7 +15,10 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
+    },
+    resetInput() {
+      this.name = "";
+    },
   }
 });
 
