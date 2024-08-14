b63d0ed17bd42efd8b4c2968351719e6f39f926d
wkchiuea <wkchiuea@gmail.com>
2024-08-14 15:15:57 +0800

    vue-S08: cmp assignment1 done

---
+++ vue/08-component-communication/02-cmp-communication-assignment1/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <active-user :username="username" :age="age"></active-user>
+    <user-data @set-user="setUser"></user-data>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      username: "",
+      age: ""
+    };
+  },
+  methods: {
+    setUser(inputUserData) {
+      this.username = inputUserData.username;
+      this.age = inputUserData.age;
+    }
+  }
+};
+</script>
+
+<style>
+.column {
+  float: left;
+  width: 50%;
+}
+</style>
---
+++ vue/08-component-communication/02-cmp-communication-assignment1/src/components/ActiveUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="column">
+    <h2>Username : {{ username }}</h2>
+    <h3>Age : {{ age }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ActiveUser",
+  props: ["username", "age"],
+}
+</script>
+
+<style scoped>
+
+</style>
