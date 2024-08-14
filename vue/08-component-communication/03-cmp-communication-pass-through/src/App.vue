535fee62311fa5c92fc67f25063c2864239c6fc3
wkchiuea <wkchiuea@gmail.com>
2024-08-14 15:41:16 +0800

    vue-S08: cmp provide+inject events

--- vue/08-component-communication/03-cmp-communication-pass-through/src/App.vue
+++ vue/08-component-communication/03-cmp-communication-pass-through/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -34,7 +34,8 @@ export default {
   },
   provide() {
     return {
-      topics: this.topics
+      topics: this.topics,
+      selectTopic: this.activateTopic
     };
   },
   methods: {
