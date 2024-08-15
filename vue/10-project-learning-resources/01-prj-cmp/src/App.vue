eb2b04e78969a3d3f1e31aa7df422fd0484b86c1
wkchiuea <wkchiuea@gmail.com>
2024-08-15 12:29:32 +0800

    vue-S10: project cmp base-card and header

--- vue/10-project-learning-resources/01-prj-cmp/src/App.vue
+++ vue/10-project-learning-resources/01-prj-cmp/src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <the-header title="Remember Me"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from "./components/learning-resources/StoredResources.vue";
+import TheHeader from "./components/layouts/TheHeader.vue";
 
 export default {
   name: "App",
   components: {
-    StoredResources
+    StoredResources,
+    TheHeader
   },
   data() {
     return {
--- vue/10-project-learning-resources/01-prj-cmp/src/main.js
+++ vue/10-project-learning-resources/01-prj-cmp/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from "./App.vue";
+import BaseCard from "./components/common/BaseCard.vue";
 
 const app = createApp(App);
+
+app.component("base-card", BaseCard);
 
 app.mount('#app');
