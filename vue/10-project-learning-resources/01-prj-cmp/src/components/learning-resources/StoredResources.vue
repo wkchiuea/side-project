c81daf766c8f783548c229def0f6b66fbd0a8986
wkchiuea <wkchiuea@gmail.com>
2024-08-17 11:04:13 +0800

    vue-S10: project cmp delete item

--- vue/10-project-learning-resources/01-prj-cmp/src/components/learning-resources/StoredResources.vue
+++ vue/10-project-learning-resources/01-prj-cmp/src/components/learning-resources/StoredResources.vue
@@ -2,6 +2,7 @@
   <ul>
     <learning-resource v-for="res in resources"
                        :key="res.id"
+                       :id="res.id"
                        :title="res.title"
                        :description="res.description"
                        :link="res.link"
--- vue/10-project-learning-resources/01-prj-cmp/src/components/learning-resources/TheResources.vue
+++ vue/10-project-learning-resources/01-prj-cmp/src/components/learning-resources/TheResources.vue
@@ -40,7 +40,8 @@ export default {
   provide() {
     return {
       resources: this.storedResources,
-      addResource: this.addResource
+      addResource: this.addResource,
+      removeResource: this.removeResource
     };
   },
   computed: {
@@ -64,6 +65,10 @@ export default {
       };
       this.storedResources.unshift(newResource);
       this.selectedTab = 'stored-resources';
+    },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(res => res.id === resId);
+      this.storedResources.splice(resIndex, 1);
     }
   }
 }
