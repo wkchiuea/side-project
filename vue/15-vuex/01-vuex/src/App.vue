2c6986ba19f090276da46f2b694c97a4ba73e686
wkchiuea <wkchiuea@gmail.com>
2024-10-03 16:53:32 +0800

    vue-S15: extract counter as component

--- vue/15-vuex/01-vuex/src/App.vue
+++ vue/15-vuex/01-vuex/src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <base-container title="Vuex">
-    <h3>{{ $store.state.counter }}</h3>
+    <the-counter></the-counter>
     <button @click="addOne">Add 1</button>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from "./components/TheCounter.vue";
 
 export default {
   components: {
     BaseContainer,
+    TheCounter
   },
-  // computed: {
-  //   counter() {
-  //     return this.$store.state.counter;
-  //   }
-  // },
   methods: {
     addOne() {
       this.$store.state.counter++;
