75cdece8cbc89bc4844b215e917ab23d6203f5fd
wkchiuea <wkchiuea@gmail.com>
2024-10-03 17:09:57 +0800

    vue-S15: add mutations

---
+++ vue/15-vuex/01-vuex/src/components/ChangeCounter.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="addOne">Add 1</button>
+</template>
+
+<script>
+export default {
+  name: "ChangeCounter",
+  methods: {
+    addOne() {
+      this.$store.commit('increment');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
